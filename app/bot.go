7ab2d8e52c7056662577ee39c1e5d36b716c016b
wangmoux <wangmoux@gmail.com>
2022-11-25 13:41:00 +0800

    update chatMap

--- app/bot.go
+++ app/bot.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/sirupsen/logrus"
-	"sync"
 	"time"
 )
 
@@ -30,45 +29,30 @@ func RunBot() {
 func updatesHandler(client Client) {
 	for update := range client.Channel() {
 		if update.Message != nil {
-			if _chatCh, ok := chatMap.Load(update.Message.Chat.ID); ok {
-				if chatCh, _ok := _chatCh.(chatChannel); _ok {
-					chatCh <- update
-					continue
-				}
+			if chatCh, ok := chatMap[update.Message.Chat.ID]; ok {
+				chatCh <- update
+				continue
 			}
 			logrus.Infof("new chat_handler=%v", update.Message.Chat.ID)
 			updateCh := make(chatChannel, 10)
-			chatMap.Store(update.Message.Chat.ID, updateCh)
+			chatMap[update.Message.Chat.ID] = updateCh
 			go chatHandler(updateCh, client.GetBot())
 			updateCh <- update
 		}
 	}
 }
 
-var chatMap sync.Map
+var chatMap = make(map[int64]chatChannel)
 
 type chatChannel chan tgbotapi.Update
 
 func chatHandler(ch chatChannel, bot *tgbotapi.BotAPI) {
-	var chatID int64
-	var ttl int64 = 600
 	for {
 		select {
 		case update := <-ch:
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 			controller.Controller(ctx, cancel, bot, update)
-			chatID = update.Message.Chat.ID
-			if update.Message.Chat.Type == "private" {
-				ttl = 60
-			} else {
-				ttl = 600
-			}
 			//go debug(bot, update)
-		case <-time.After(time.Second * time.Duration(ttl)):
-			logrus.Infof("close chat_handler=%v", chatID)
-			chatMap.Delete(chatID)
-			close(ch)
-			return
 		}
 	}
 }
